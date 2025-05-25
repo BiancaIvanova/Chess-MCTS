d9e2f93ab7ea0af4918ef67f55a2c798b6502247
Bianca Ivanova <bianca.ivanova.1.0@gmail.com>
2025-05-25 23:58:21 +0100

    Initial working endpoint

---
+++ src/main/java/project/chess/ChessApplication.java
@@ -0,0 +1,24 @@
+package project.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class}) // disable database for now
+@RestController
+public class ChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot()
+	{
+		// GET endpoint
+		return "Hello World!";
+	}
+
+}

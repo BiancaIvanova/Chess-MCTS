0f9c0f7e45e17114c690334cfecd3d8ab492e487
Bianca Ivanova <bianca.ivanova.1.0@gmail.com>
2025-05-26 23:56:44 +0100

    Application properties configured using environment variables

--- src/main/java/project/chess/ChessApplication.java
+++ src/main/java/project/chess/ChessApplication.java
@@ -2,23 +2,13 @@ package project.chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class}) // disable database for now
-@RestController
+@SpringBootApplication
+
 public class ChessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String apiRoot()
-	{
-		// GET endpoint
-		return "Hello World!";
 	}
 
 }

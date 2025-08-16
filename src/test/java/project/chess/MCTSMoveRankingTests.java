d3cbe963051d7e8120b6904740be49049f4f893e
Bianca Ivanova <bianca.ivanova.1.0@gmail.com>
2025-08-16 19:24:01 +0100

    Updated POM file, removing redundant dependencies

--- src/test/java/project/chess/MCTSMoveRankingTests.java
+++ src/test/java/project/chess/MCTSMoveRankingTests.java
@@ -113,6 +113,7 @@ public class MCTSMoveRankingTests {
         String fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w KQkq - 0 1";
         Tree<MCTSData> tree = buildTreeFromFEN(fen);
         runMCTSWithMemoryUsage(tree, 500);
+
     }
 
     @Test
